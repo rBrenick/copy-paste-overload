6802fe93ac43ab9a8f6827a5831d9ff8115d9b66
rBrenick <richard.brenick@embark-studios.com>
2021-10-14 11:12:43 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -19,10 +19,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    
-    install_requires=[
-    "Qt.py",
-    ]
 )
 
 
